ed6300c56709bc89ce03428919c218e7cb289103
bustamd1 <bustamd1@.(none)>
2013-08-29 12:16:32 -0400

    -moved core projects to external lib MMG.Core -updated references

---
+++ src/MMG.Core.Persistence/IUnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿// *************************************************
+// MMG.Core.Persistence.IUnitOfWork.cs
+// Last Modified: 08/29/2013 12:13 PM
+// Modified By: Bustamante, Diego (bustamd1)
+// *************************************************
+
+using System.Data;
+
+namespace MMG.Core.Persistence
+{
+    public interface IUnitOfWork
+    {
+        bool IsInTransaction { get; }
+
+        void SaveChanges();
+
+        void BeginTransaction();
+
+        void BeginTransaction(IsolationLevel isolationLevel);
+
+        void RollBackTransaction();
+
+        void CommitTransaction();
+    }
+}
---
+++ src/MMG.Core.Query/CompositeSpecification.cs
@@ -0,0 +1,41 @@
+﻿// *************************************************
+// MMG.Core.Query.CompositeSpecification.cs
+// Last Modified: 08/29/2013 12:12 PM
+// Modified By: Bustamante, Diego (bustamd1)
+// *************************************************
+
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace MMG.Core.Query
+{
+    /// <summary>
+    /// http://devlicio.us/blogs/jeff_perrin/archive/2006/12/13/the-specification-pattern.aspx
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    public abstract class CompositeSpecification<TEntity> : ISpecification<TEntity>
+    {
+        protected readonly Specification<TEntity> _leftSide;
+        protected readonly Specification<TEntity> _rightSide;
+
+        public CompositeSpecification(Specification<TEntity> pLeftSide, Specification<TEntity> pRightSide)
+        {
+            _leftSide = pLeftSide;
+            _rightSide = pRightSide;
+        }
+
+        public abstract Expression<Func<TEntity, bool>> Predicate { get; }
+
+        public virtual TEntity SatisfyingEntityFrom(IQueryable<TEntity> pQuery)
+        {
+            //TODO: test single instead of firstordefault()
+            return SatisfyingEntitiesFrom(pQuery).SingleOrDefault();
+        }
+
+        public virtual IQueryable<TEntity> SatisfyingEntitiesFrom(IQueryable<TEntity> pQuery)
+        {
+            return pQuery.Where(Predicate);
+        }
+    }
+}
