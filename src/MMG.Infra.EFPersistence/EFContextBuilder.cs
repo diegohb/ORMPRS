a0585266520fd88745f0a74e470069a4cae20ed0
bustamd1 <bustamd1@.(none)>
2013-08-29 15:11:04 -0400

    added EF contextbuilder and efdbcontext wrapper class.

---
+++ src/MMG.Infra.EFPersistence/EFContextBuilder.cs
@@ -0,0 +1,137 @@
+﻿// *************************************************
+// MMG.Infra.EFPersistence.EFContextBuilder.cs
+// Last Modified: 08/29/2013 3:03 PM
+// Modified By: Bustamante, Diego (bustamd1)
+// *************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Objects;
+using System.Reflection;
+using MMG.Core.Persistence;
+
+namespace MMG.Infra.EFPersistence
+{
+    public class EFContextBuilder<TContext> : DbModelBuilder, IDbContextBuilder<TContext>
+        where TContext : DbContext, IDbContext
+    {
+        private readonly DbProviderFactory _factory;
+        private readonly ConnectionStringSettings _cnStringSettings;
+        private readonly bool _recreateDatabaseIfExists;
+        private readonly bool _lazyLoadingEnabled;
+
+        public EFContextBuilder(string pConnectionStringName, EFContextConfiguration pContextConfig)
+        {
+            _cnStringSettings = ConfigurationManager.ConnectionStrings[pConnectionStringName];
+            _factory = DbProviderFactories.GetFactory(_cnStringSettings.ProviderName);
+            _recreateDatabaseIfExists = pContextConfig.RecreateDatabaseIfExists;
+            _lazyLoadingEnabled = pContextConfig.LazyLoadingEnabled;
+
+            addConfigurations(pContextConfig.MappingAssemblies);
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="TContext"/>.
+        /// </summary>
+        /// <returns></returns>
+        public TContext BuildDbContext()
+        {
+            var cn = _factory.CreateConnection();
+            cn.ConnectionString = _cnStringSettings.ConnectionString;
+
+            var dbModel = base.Build(cn);
+
+            var ctx = dbModel.Compile().CreateObjectContext<ObjectContext>(cn);
+            ctx.ContextOptions.LazyLoadingEnabled = _lazyLoadingEnabled;
+
+            if (!ctx.DatabaseExists())
+            {
+                ctx.CreateDatabase();
+            }
+            else if (_recreateDatabaseIfExists)
+            {
+                ctx.DeleteDatabase();
+                ctx.CreateDatabase();
+            }
+
+            return (TContext) new DbContext(ctx, true);
+        }
+
+        /// <summary>
+        /// Adds mapping classes contained in provided assemblies and register entities as well
+        /// </summary>
+        /// <param name="pMappingAssemblies"></param>
+        private void addConfigurations(ICollection<string> pMappingAssemblies)
+        {
+            if (pMappingAssemblies == null || pMappingAssemblies.Count == 0)
+            {
+                throw new ArgumentNullException("pMappingAssemblies", "You must specify at least one mapping assembly");
+            }
+
+            var hasMappingClass = false;
+            foreach (var mappingAssembly in pMappingAssemblies)
+            {
+                var asm = Assembly.LoadFrom(makeLoadReadyAssemblyName(mappingAssembly));
+
+                foreach (Type type in asm.GetTypes())
+                {
+                    if (!type.IsAbstract)
+                    {
+                        if (type.BaseType.IsGenericType && IsMappingClass(type.BaseType))
+                        {
+                            hasMappingClass = true;
+
+                            // http://areaofinterest.wordpress.com/2010/12/08/dynamically-load-entity-configurations-in-ef-codefirst-ctp5/
+                            dynamic configurationInstance = Activator.CreateInstance(type);
+                            Configurations.Add(configurationInstance);
+                        }
+                    }
+                }
+            }
+
+            if (!hasMappingClass)
+            {
+                throw new ArgumentException("No mapping class found!");
+            }
+        }
+
+        /// <summary>
+        /// Determines whether a type is a subclass of entity mapping type
+        /// </summary>
+        /// <param name="mappingType">Type of the mapping.</param>
+        /// <returns>
+        /// 	<c>true</c> if it is mapping class; otherwise, <c>false</c>.
+        /// </returns>
+        private bool IsMappingClass(Type mappingType)
+        {
+            Type baseType = typeof (EntityTypeConfiguration<>);
+            if (mappingType.GetGenericTypeDefinition() == baseType)
+            {
+                return true;
+            }
+            if ((mappingType.BaseType != null) &&
+                !mappingType.BaseType.IsAbstract &&
+                mappingType.BaseType.IsGenericType)
+            {
+                return IsMappingClass(mappingType.BaseType);
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Ensures the assembly name is qualified
+        /// </summary>
+        /// <param name="pAssemblyName">The name of the assembly to fix.</param>
+        /// <returns></returns>
+        private static string makeLoadReadyAssemblyName(string pAssemblyName)
+        {
+            return (!pAssemblyName.EndsWith(".dll"))
+                       ? pAssemblyName.Trim() + ".dll"
+                       : pAssemblyName.Trim();
+        }
+    }
+}
