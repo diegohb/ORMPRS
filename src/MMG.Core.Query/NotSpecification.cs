7fbc80bb909a9d30e1396dc9122435c38178746d
bustamd1 <bustamd1@.(none)>
2013-08-29 16:37:14 -0400

    added NotSpec

---
+++ src/MMG.Core.Query/NotSpecification.cs
@@ -0,0 +1,20 @@
+﻿// *************************************************
+// MMG.Core.Query.NotSpecification.cs
+// Last Modified: 08/29/2013 4:38 PM
+// Modified By: Bustamante, Diego (bustamd1)
+// *************************************************
+
+using System;
+using System.Linq.Expressions;
+
+namespace MMG.Core.Query
+{
+    public class NotSpecification<TEntity> : Specification<TEntity>
+    {
+        public NotSpecification(Expression<Func<TEntity, bool>> pPredicate)
+            : base((Expression<Func<TEntity, bool>>) pPredicate.Not()) {}
+
+        public NotSpecification(Specification<TEntity> pPredicate)
+            : base((Expression<Func<TEntity, bool>>) pPredicate.Predicate.Not()) {}
+    }
+}
