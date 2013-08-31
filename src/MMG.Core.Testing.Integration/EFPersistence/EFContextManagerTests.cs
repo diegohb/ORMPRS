90ea5aa9797c5445b87c3d22a3f2c7f7e50d583a
bustamd1 <bustamd1@.(none)>
2013-08-31 11:28:58 -0400

    -added EFPersistence dir -added ef dbcontextmanager test -installed nunit nuget -installed ef5 nuget (net40)

---
+++ src/MMG.Core.Testing.Integration/EFPersistence/EFContextManagerTests.cs
@@ -0,0 +1,51 @@
+﻿// *************************************************
+// MMG.Core.Testing.Integration.EFContextManagerTests.cs
+// Last Modified: 08/31/2013 11:26 AM
+// Modified By: Bustamante, Diego (bustamd1)
+// *************************************************
+
+using System;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using MMG.Core.Persistence;
+using MMG.Core.Persistence.Impl;
+using MMG.Infra.EFPersistence;
+using NUnit.Framework;
+
+namespace MMG.Core.Testing.Integration.EFPersistence
+{
+    [TestFixture]
+    public class EFContextManagerTests
+    {
+        [Test]
+        public void InitializeManager()
+        {
+            doAction(() => initializeStorage());
+            Assert.IsNull(EFContextManager.Instance.CurrentFor("TechnicalAssistanceDB"));
+            EFContextManager.Instance.AddContextBuilder("TechnicalAssistanceDB", new EFContextConfiguration(new[] { "LGCYDAPI.Testing.Integration" }));
+            var dbContext = EFContextManager.Instance.CurrentFor("TechnicalAssistanceDB");
+            Assert.IsNotNull(dbContext);
+            Assert.IsInstanceOf<EFDbContext>(dbContext);
+        }
+
+        private static void initializeStorage()
+        {
+            Assert.IsNull(EFContextManager.Instance.Storage);
+            var storage = new SimpleDbContextStorage();
+            EFContextManager.Instance.InitStorage(storage);
+            Assert.IsNotNull(EFContextManager.Instance.Storage);
+            Assert.IsInstanceOf<IDbContextStorage>(EFContextManager.Instance.Storage);
+        }
+
+        protected static void doAction(Expression<Action> pAction)
+        {
+            Console.Write("Executing {0} ... ", pAction.Body);
+            var timer = new Stopwatch();
+            var act = pAction.Compile();
+            timer.Start();
+            act.Invoke();
+            timer.Stop();
+            Console.WriteLine("Executed successfully in {0} ms.", timer.ElapsedMilliseconds);
+        }
+    }
+}
