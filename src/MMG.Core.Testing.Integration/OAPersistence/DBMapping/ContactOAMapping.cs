28230580388d58cb75a1de95ec32b41f652175a6
bustamd1 <bustamd1@.(none)>
2013-09-03 11:09:35 -0400

    added OA mapping for tests.

---
+++ src/MMG.Core.Testing.Integration/OAPersistence/DBMapping/ContactOAMapping.cs
@@ -0,0 +1,22 @@
+﻿// *************************************************
+// MMG.Core.Testing.Integration.ContactOAMapping.cs
+// Last Modified: 08/31/2013 5:28 PM
+// Modified By: Bustamante, Diego (bustamd1)
+// *************************************************
+
+using MMG.Core.Persistence;
+using MMG.Core.Testing.Integration.Northwind;
+
+namespace MMG.Core.Testing.Integration.OAPersistence.DBMapping
+{
+    public class ContactOAMapping : IMapEntityToDb<Contact>
+    {
+        public ContactOAMapping()
+        {
+            /*Property(p => p.Name).HasColumnName("ContactName").HasMaxLength(30);
+            Property(p => p.Title).HasColumnName("ContactTitle").HasMaxLength(30);
+            Property(p => p.Phone).HasColumnName("Phone").HasMaxLength(24);
+            Property(p => p.Fax).HasColumnName("Fax").HasMaxLength(24);*/
+        }
+    }
+}
---
+++ src/MMG.Core.Testing.Integration/OAPersistence/DBMapping/CustomerOAMapping.cs
@@ -0,0 +1,37 @@
+// *************************************************
+// MMG.Core.Testing.Integration.CustomerOAMapping.cs
+// Last Modified: 08/31/2013 5:28 PM
+// Modified By: Bustamante, Diego (bustamd1)
+// *************************************************
+
+using MMG.Core.Persistence;
+using MMG.Core.Testing.Integration.Northwind;
+using Telerik.OpenAccess.Metadata.Fluent;
+
+namespace MMG.Core.Testing.Integration.OAPersistence.DBMapping
+{
+    public class CustomerOAMapping : MappingConfiguration<Customer>, IMapEntityToDb<Customer>
+    {
+        public CustomerOAMapping()
+        {
+            MapType().ToTable("Customers");
+
+            HasProperty(p => p.Id).HasFieldName("CustomerID").IsIdentity().HasLength(5);
+            HasProperty(p => p.Name).HasFieldName("CompanyName").HasLength(40).IsNullable();
+
+            //contact complex
+            HasProperty(p => p.Contact.Name).HasFieldName("ContactName").HasLength(30);
+            HasProperty(p => p.Contact.Title).HasFieldName("ContactTitle").HasLength(30);
+            HasProperty(p => p.Contact.Phone).HasFieldName("Phone").HasLength(24);
+            HasProperty(p => p.Contact.Fax).HasFieldName("Fax").HasLength(24);
+
+            //address complex
+            HasProperty(p => p.Contact.Address.Street).HasFieldName("Address").HasLength(60);
+            HasProperty(p => p.Contact.Address.City).HasFieldName("City").HasLength(15);
+            HasProperty(p => p.Contact.Address.Region).HasFieldName("Region").HasLength(15);
+            HasProperty(p => p.Contact.Address.PostalCode).HasFieldName("PostalCode").HasLength(60);
+            HasProperty(p => p.Contact.Address.Country).HasFieldName("Country").HasLength(15);
+        }
+
+    }
+}
