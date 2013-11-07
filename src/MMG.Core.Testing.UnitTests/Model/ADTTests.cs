69cf31823797f26f4b25277ea6ccde24f9334512
bustamd1 <bustamd1@.(none)>
2013-11-07 18:44:51 -0500

    added Unit Tests project. added adtTests fixture placeholder

---
+++ src/MMG.Core.Testing.UnitTests/Model/ADTTests.cs
@@ -0,0 +1,13 @@
+﻿// *************************************************
+// MMG.Core.Testing.UnitTests.Class1.cs
+// Last Modified: 11/07/2013 6:44 PM
+// Modified By: Bustamante, Diego (bustamd1)
+// *************************************************
+
+namespace MMG.Core.Testing.UnitTests.Model
+{
+    public class ADTTests
+    {
+        
+    }
+}
