202ba87dedb2d944cccd147fa58f1a786aa159ba
bustamd1 <bustamd1@.(none)>
2013-11-07 19:39:40 -0500

    -added ref to core.model -implemente IConvertible and ICOmparable<T> on adtbase. -added unit test

--- src/MMG.Core.Model/ADTBase.cs
+++ src/MMG.Core.Model/ADTBase.cs
@@ -4,6 +4,9 @@
 // Modified By: Bustamante, Diego (bustamd1)
 // *************************************************
 
+using System;
+using System.Globalization;
+
 namespace MMG.Core.Model
 {
     /// <summary>
@@ -14,7 +17,7 @@ namespace MMG.Core.Model
     /// http://programmers.stackexchange.com/questions/148747/abstract-data-type-and-data-structure or
     /// http://en.wikipedia.org/wiki/Abstract_data_type
     /// </remarks>
-    public abstract class ADTBase<TUnderlyingType>
+    public abstract class ADTBase<TUnderlyingType> : IConvertible, IComparable<TUnderlyingType>
         where TUnderlyingType : struct
     {
         public ADTBase() {}
@@ -31,9 +34,104 @@ namespace MMG.Core.Model
             return pAbtractDataTypeObject.ToString();
         }
 
+        public virtual int CompareTo(TUnderlyingType pOther)
+        {
+            throw new NotImplementedException();
+        }
+
         public override string ToString()
+        {
+            return ToString(CultureInfo.InvariantCulture);
+        }
+
+        #region IConvertible
+
+        public virtual TypeCode GetTypeCode()
+        {
+            return Type.GetTypeCode(Value.GetType());
+        }
+
+        public virtual bool ToBoolean(IFormatProvider provider)
+        {
+            return Convert.ToBoolean(Value, provider);
+        }
+
+        public virtual char ToChar(IFormatProvider provider)
+        {
+            return Convert.ToChar(Value, provider);
+        }
+
+        public virtual sbyte ToSByte(IFormatProvider provider)
+        {
+            return Convert.ToSByte(Value, provider);
+        }
+
+        public virtual byte ToByte(IFormatProvider provider)
+        {
+            return Convert.ToByte(Value, provider);
+        }
+
+        public virtual short ToInt16(IFormatProvider provider)
+        {
+            return Convert.ToInt16(Value, provider);
+        }
+
+        public virtual ushort ToUInt16(IFormatProvider provider)
+        {
+            return Convert.ToUInt16(Value, provider);
+        }
+
+        public virtual int ToInt32(IFormatProvider provider)
+        {
+            return Convert.ToInt32(Value, provider);
+        }
+
+        public virtual uint ToUInt32(IFormatProvider provider)
+        {
+            return Convert.ToUInt32(Value, provider);
+        }
+
+        public virtual long ToInt64(IFormatProvider provider)
+        {
+            return Convert.ToInt64(Value, provider);
+        }
+
+        public virtual ulong ToUInt64(IFormatProvider provider)
+        {
+            return Convert.ToUInt64(Value, provider);
+        }
+
+        public virtual float ToSingle(IFormatProvider provider)
+        {
+            return Convert.ToSingle(Value, provider);
+        }
+
+        public virtual double ToDouble(IFormatProvider provider)
+        {
+            return Convert.ToDouble(Value, provider);
+        }
+
+        public virtual decimal ToDecimal(IFormatProvider provider)
+        {
+            return Convert.ToDecimal(Value, provider);
+        }
+
+        public virtual DateTime ToDateTime(IFormatProvider provider)
+        {
+            return Convert.ToDateTime(Value, provider);
+        }
+
+        public virtual string ToString(IFormatProvider provider)
         {
             return Value.ToString();
         }
+
+        public virtual object ToType(Type conversionType, IFormatProvider provider)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
     }
 }
--- src/MMG.Core.Testing.UnitTests/Model/ADTTests.cs
+++ src/MMG.Core.Testing.UnitTests/Model/ADTTests.cs
@@ -4,18 +4,42 @@
 // Modified By: Bustamante, Diego (bustamd1)
 // *************************************************
 
+using System;
+using System.ComponentModel;
+using MMG.Core.Model;
 using NUnit.Framework;
 
 namespace MMG.Core.Testing.UnitTests.Model
 {
+    /// <summary>
+    /// Tests for adt types.
+    /// </summary>
+    /// <remarks>See more:
+    /// http://msdn.microsoft.com/en-us/library/system.componentmodel.typeconverter(v=vs.110).aspx
+    /// http://msdn.microsoft.com/en-us/library/98bbex99(v=vs.100).aspx
+    /// </remarks>
     [TestFixture]
     public class ADTTests
     {
+        class TestIntegerADT : ADTBase<int>
+        {
+            public TestIntegerADT(int pValue) : base(pValue) { }
+
+            public override int Value { get; set; }
+        }
+
         [Test]
         public void ADTConverter_ShouldConvertToUnderlyingType()
         {
-            
+            var adt = new TestIntegerADT(5);
+            var typeConverter = TypeDescriptor.GetConverter(typeof(TestIntegerADT));
+            Assert.IsNotNull(typeConverter);
+            var actualValue = Convert.ToInt32(adt);
+            //var actualValue = typeConverter.ConvertTo(adt, typeof(int)) as int?;
+            //Assert.True(actualValue);
+            // ReSharper disable once PossibleInvalidOperationException
+            Assert.AreEqual(5, actualValue);
         }
-        
+
     }
 }
