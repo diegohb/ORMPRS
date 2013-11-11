9c0c9b1b41f276c5c60aaa2e59cb7ccb69034354
bustamd1 <bustamd1@.(none)>
2013-11-11 16:03:42 -0500

    add ef generic repo test

---
+++ src/MMG.Core.Testing.Integration/EFPersistence/EFGenericRepoTests.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq;
+using MMG.Core.Persistence;
+using MMG.Core.Persistence.Impl;
+using MMG.Core.Testing.Integration.Northwind;
+using MMG.Infra.EFPersistence;
+using NUnit.Framework;
+
+namespace MMG.Core.Testing.Integration.EFPersistence
+{
+    [TestFixture]
+    public class EFGenericRepoTests
+    {
+        private NorthwindDB _nwDB;
+        private EFGenericRepository _repo;
+
+        [SetUp]
+        public void Init()
+        {
+            initializeStorage();
+            configureNorthwindContext();
+            Assert.IsNotNull(_nwDB);
+        }
+
+        [TearDown]
+        public void Destroy()
+        {
+            _nwDB.ShippersSet.Where(p => p.Name.Contains("nUnit")).ToList().ForEach(pShipper => _nwDB.ShippersSet.Remove(pShipper));
+            _nwDB.SaveChanges();
+        }
+
+        [Test]
+        public void FetchShippersShouldMatch()
+        {
+            //ARRANGE
+            var expected = _nwDB.ShippersSet.ToList();
+
+            //ACT
+            var actual = _repo.GetAll<Shipper>().ToList();
+
+            //ASSERT
+            CollectionAssert.AreEquivalent(expected, actual);
+        }
+
+
+        private static void initializeStorage()
+        {
+            Assert.IsNull(EFContextManager.Instance.Storage);
+            var storage = new SimpleDbContextStorage();
+            EFContextManager.Instance.InitStorage(storage);
+            Assert.IsNotNull(EFContextManager.Instance.Storage);
+            Assert.IsInstanceOf<IDbContextStorage>(EFContextManager.Instance.Storage);
+        }
+
+        private void configureNorthwindContext()
+        {
+            EFContextManager.Instance.AddContextBuilder(Utility.NorthwindDBConnectionName, new EFContextConfiguration(new[] { "MMG.Core.Testing.Integration" }));
+            var dbContext = EFContextManager.Instance.CurrentFor(Utility.NorthwindDBConnectionName);
+            Assert.IsNotNull(dbContext);
+            Assert.IsInstanceOf<EFDbContext>(dbContext);
+
+            _nwDB = new NorthwindDB();
+            _repo = new EFGenericRepository((EFDbContext)dbContext);
+        }
+
+    }
+}
