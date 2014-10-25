57a0f777b2a24d87923ea9af7380075c18cd5364
bustamd1 <bustamd1@.(none)>
2014-10-25 18:46:27 -0400

    -added interface IProvideConnectionString -added impl fo ConnectionStringProvider

---
+++ src/MMG.Core.Persistence/IProvideConnectionString.cs
@@ -0,0 +1,13 @@
+﻿// *************************************************
+// MMG.Core.Persistence.IProvideConnectionString.cs
+// Last Modified: 10/25/2014 6:42 PM
+// Modified By: Bustamante, Diego (bustamd1)
+// *************************************************
+
+namespace MMG.Core.Persistence
+{
+    public interface IProvideConnectionString
+    {
+        string ConnectionString { get; }
+    }
+}
---
+++ src/MMG.Core.Persistence/Impl/ConnectionStringProvider.cs
@@ -0,0 +1,23 @@
+﻿// *************************************************
+// MMG.Core.Persistence.ConnectionStringProvider.cs
+// Last Modified: 10/25/2014 6:46 PM
+// Modified By: Bustamante, Diego (bustamd1)
+// *************************************************
+
+namespace MMG.Core.Persistence.Impl
+{
+    public class ConnectionStringProvider : IProvideConnectionString
+    {
+        private readonly string _connectionString;
+
+        public ConnectionStringProvider(string pConnectionString)
+        {
+            _connectionString = pConnectionString;
+        }
+
+        public string ConnectionString
+        {
+            get { return _connectionString; }
+        }
+    }
+}
